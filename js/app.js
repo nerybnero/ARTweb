7fe930ac0f507db94728a73baba80eeaf899383b
GRblSHA <dron-skm1234@mail.ru>
2023-11-13 13:08:18 +0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,10 @@
+import {Click} from "./module/Click.js"
+
+const menu = new Click({
+    target: '.menu__icon',
+    parent: '.menu',
+    class: 'menu-open',
+    lock: true
+})
+
+menu.listenEvent()
